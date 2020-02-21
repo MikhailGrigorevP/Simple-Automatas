208495a231c92920b0ea33f5b660a35759176b7f
Mikhail <16112000m@gmail.com>
2020-02-21 20:17:58 +0300

    First

---
+++ generator.py
@@ -0,0 +1,98 @@
+from collections import Counter
+import string
+import random
+
+
+class Generator:
+    # атрибуты класса
+
+    # методы класса
+
+    # создание методов класса
+    def __init__(self, num=1000000):
+
+        # количество элементов
+        self.__n = num
+        self.__A = []
+        # файл со всеми строками функций
+        try:
+            self. __f = open('functions.txt')
+        except IOError as e:
+            self.__f = open('functions.txt', 'w')
+            self.generate_file()
+
+    def __del__(self):
+        self.__f.close()
+
+    def get_num(self):
+        return self.__n
+
+    def get_file_content(self):
+        try:
+            f = open('functions.txt')
+        except IOError as e:
+            self.generate_file()
+            f = open('functions.txt')
+
+        nf = f.read()
+        self.__A = nf.split('\n')
+
+        f.close()
+        return self.__A
+
+    def damage(self):
+        return random.random()
+
+    # генерировать тип
+    def generate_type(self):
+        type_name = ['int', 'short', 'long']
+        func_type = type_name[random.randrange(3)]
+        if self.damage() > 0.90:
+            func_type = ''.join(random.choice(string.ascii_letters) for _ in range(4))
+        else:
+            func_type = type_name[random.randrange(3)]
+        return func_type
+
+    # генерировать имя функции
+    def generate_func_name(self):
+        func_name_length = random.randrange(15) + 1
+        if self.damage() > 0.90:
+            first_sym = random.choice(string.digits)
+        else:
+            first_sym = random.choice(string.ascii_letters)
+        return first_sym +\
+            ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(func_name_length))
+
+    # генерировать список параметров
+    def generate_options_list(self):
+        generate_options_num = random.randrange(10)
+        symbols = ['(', ')', ', ', ';', ' ']
+
+        if self.damage() < 0.90:
+           first_symbol = symbols[0]
+           last_symbol = symbols[1]
+        else:
+           first_symbol = symbols[random.randrange(4)]
+           last_symbol = symbols[random.randrange(4)]
+
+        return first_symbol + \
+            ''.join((self.generate_type() + ' ' + self.generate_func_name() +
+                     ((', ' if (self.damage() < 0.80) else symbols[random.randrange(4)])
+                      if (i < generate_options_num - 1) else '')) for i in range(generate_options_num)) +\
+            ''.join(last_symbol)
+
+    # генерировать строку
+    def generate_string(self):
+        symbols = ['(', ')', ',', ';', ' ']
+        if self.damage() < 0.90:
+            last_symbol = ';'
+        else:
+            last_symbol = symbols[random.randrange(4)]
+        return ''.join(self.generate_type() + ' ' + self.generate_func_name() + ' ' + self.generate_options_list() + last_symbol)
+
+    # генерировать файл
+    def generate_file(self):
+        for _ in range(self.__n):
+            self.__f.write(self.generate_string() + '\n')
+
+
---
+++ task1.py
@@ -0,0 +1,63 @@
+import generator
+import re
+import time
+
+class Recognizer:
+
+    __A = []
+    __Over_A = []
+
+    def __init__(self, strings):
+        self.__f = open('result1.txt', 'w')
+        self.__strings = strings
+
+    def __del__(self):
+        self.__f.close()
+
+    def check_strings(self):
+        f_time = open('time1.txt', 'w')
+        f_time.write('iter time' + '\n')
+        f_time = open('time1.txt', 'a')
+        start_time = time.perf_counter()
+        for i in range(len(self.__strings) - 1):
+            if (re.match(r'((^(int|long|short))(\s+)(([^0-9]{1}[0-9a-zA-Z]{0,15})))((\s+)(\()((\s*)'
+                                   r'(int|long|short)(\s+)([^0-9]{1}[0-9a-zA-Z]{0,15})((\,)?))*(\)))(\;$)',
+                                   self.__strings[i])):
+                self.__f.write(self.__strings[i] + ' - yes' + '\n')
+                self.__Over_A.append((self.__strings[i].split())[1])
+            else:
+                self.__f.write(self.__strings[i] + ' - no' + '\n')
+        f_time.write(str(i) + ' ' + str(time.perf_counter() - start_time)+'\n')
+        f_time.close()
+
+    def get_file_content(self):
+        try:
+            self.__f = open('result1.txt')
+        except IOError as e:
+            self.check_strings()
+            self.__f = open('result1.txt')
+
+        nf = self.__f.read()
+        self.__A = nf.split('\n')
+
+        self.__f.close()
+        return self.__A
+
+    def analyze_overload(self):
+        counter_one = generator.Counter(self.__Over_A )
+        f_overload = open('overload.txt', 'w')
+        for item, count in counter_one.items():
+            if count > 1:
+                f_overload.write(str(item) + ' - ' + str(count) + '\n')
+        f_overload.close()
+
+
+if __name__ == "__main__":
+
+    all_strings = generator.Generator().get_file_content()
+    recognizer = Recognizer(all_strings)
+
+    recognizer.check_strings()
+    A = recognizer.get_file_content()
+    recognizer.analyze_overload()
+
